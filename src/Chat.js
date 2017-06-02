ef656cdc346c29542c3b2b03544947b2e4eca978
harishankards <harishankards@gmail.com>
2017-06-02 16:29:05 +0530

    Add component chat

---
+++ src/Chat.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Chat extends Component{
+  render(){
+    return(
+      <div>
+        This is the chat Component!!
+      </div>
+    );
+  }
+}
+
+export default Chat;
