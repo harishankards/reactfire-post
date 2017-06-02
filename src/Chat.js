6520d2d3f238f978162ad61fca1047509a9147fb
harishankards <harishankards@gmail.com>
2017-06-02 17:19:54 +0530

    add event and working code for local chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,46 @@
 import React, {Component} from 'react';
 
 class Chat extends Component{
+  constructor(props,context){
+    super(props, context)
+    this.updateMessage = this.updateMessage.bind(this)
+    this.submitMessage = this.submitMessage.bind(this)
+    this.state = {
+      message: '',
+      messages: [
+      ]
+    }
+  }
+  updateMessage(event){
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  submitMessage(event){
+    const nextMessage = {
+      id: this.state.messages.length,
+      text: this.state.message
+    }
+    var list= Object.assign([],this.state.messages)
+    list.push(nextMessage)
+    this.setState({
+      messages: list
+    })
+  }
   render(){
+    const currentMessage = this.state.messages.map((message) => {
+        return(
+          <li key={message.id}>{message.text}</li>
+        )
+    })
     return(
       <div>
-        This is the chat Component!!
+        <ol>
+          {currentMessage}
+        </ol>
+          <input onChange={this.updateMessage} type="text" placeholder="Enter your message..." />
+          <button onClick={this.submitMessage}> Submit </button>
       </div>
     );
   }
